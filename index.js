e4c445e3cb159e567768d44b1a189bfbd17959a8
boyraf <raphael.kymotho@gmail.com>
2023-03-14 12:21:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
+  let result;
+  if (a <= 400){
+   return result = 'This one is on me!'
+  }
+  else if (a > 2000){
+   return result = 'I will gladly take your thirty bucks..'
+  }
+  else (a > 2500);{
+   return result ='No can do.'
+  }
+  return result;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(b){
+  let result;
+  if (b = 'NYC'){
+    return result = 'Ok, sounds good.'
+  }
+  else {
+    return result = 'No go.'
+  }
+  return result;
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(c){
+  let result;
+  if (c = 'generous'){
+    return result = 'Thank you so much.'
+  }
+  else if (c = 'not as generous'){
+    return result = 'Thank you.'
+  }
+  else {
+    return result = 'Bye.'
+  }
+  return result;
   // Write your code here!
 }
